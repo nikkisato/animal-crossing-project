4a63a849fb9fb762916a3d475918b7c31a114b45
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2024-07-15 18:06:43 -0700

    Getting the folder structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+// src/components/Header/Header.tsx
+import React from 'react';
+
+function Header() {
+	return (
+		<header>
+			<div>Menu Icon{/* // Menu Icon */}</div>
+			<div>Logo{/* // Logo */}</div>
+			<div>Search Icon{/* // Search Icon This opens a Modal */}</div>
+		</header>
+	);
+}
+
+export default Header;
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+// import { Header } from '@components';
+
+export * from './Header';
